5f7c041eb06aa8542a22b9106e5d99c69612742e
jw <jdb0731@naver.com>
2023-01-03 20:57:12 +0900

    post 추가

--- src/main/java/jwj/jwjtest/controller/TestControllerV1.java
+++ src/main/java/jwj/jwjtest/controller/TestControllerV1.java
@@ -2,6 +2,7 @@ package jwj.jwjtest.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,5 +12,10 @@ public class TestControllerV1 {
 	@GetMapping
 	public String hello() {
 		return "hello world!";
+	}
+	
+	@PostMapping
+	public String helloPost() {
+		return "redirect:/basic/hello";
 	}
 }
