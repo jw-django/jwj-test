a9327adcb06ac6e629710d148a97527cf7e38b0a
jw <jdb0731@naver.com>
2023-01-03 20:09:23 +0900

    최초 커밋!

---
+++ src/main/java/jwj/jwjtest/JwjTestApplication.java
@@ -0,0 +1,13 @@
+package jwj.jwjtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwjTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwjTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jwj/jwjtest/JwjTestApplicationTests.java
@@ -0,0 +1,13 @@
+package jwj.jwjtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwjTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
