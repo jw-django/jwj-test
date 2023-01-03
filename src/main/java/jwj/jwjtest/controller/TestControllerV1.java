97133e76b167420edef7b194787935f8f6c7c05c
jw <jdb0731@naver.com>
2023-01-03 20:52:03 +0900

    컨트롤러 추가

---
+++ src/main/java/jwj/jwjtest/controller/TestControllerV1.java
@@ -0,0 +1,15 @@
+package jwj.jwjtest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestControllerV1 {
+
+	@ResponseBody
+	@GetMapping
+	public String hello() {
+		return "hello world!";
+	}
+}
